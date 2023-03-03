d38d76b7215d8223cc90b36db2b50d31c96075d1
cereceda1991 <cereceda1991@gmail.com>
2023-03-03 00:51:04 -0500

    modify

--- src/components/ProductInfo/SliderImg.jsx
+++ src/components/ProductInfo/SliderImg.jsx
@@ -1,66 +1,3 @@
-// import React, { useState } from 'react';
-// import './styles/sliderImg.css';
-
-// const SliderImg = ({ listImgs }) => {
-//   const [indexImg, setIndexImg] = useState(0);
-
-//   const styleContainer = {
-//     transform: `translateX(calc(100% * -${indexImg}/3))`,
-//   };
-
-//   const handleBack = () => {
-
-//     if (indexImg - 1 < 0) {
-//       setIndexImg(2);
-//     } else {
-//       setIndexImg(indexImg - 1);
-//     }
-//   };
-
-//   const handleNext = () => {
-
-//     if (indexImg + 1 > 2) {
-//       setIndexImg(0);
-//     } else {
-//       setIndexImg(indexImg + 1);
-//     }
-//   };
-
-//   console.log(listImgs);
-
-//   return (
-//     <div className="slider">
-//       <button onClick={handleBack} className="slider-back">
-//         <i className="fa-solid fa-chevron-left"></i>
-//       </button>
-//       <div style={styleContainer} className="slider-container">
-//         {listImgs?.map((url) => (
-//           <div className="slider-img-container" key={url}>
-//             <img className="slider-img" src={url} alt="product-img" />
-//           </div>
-//         ))}
-//       </div>
-//       <button onClick={handleNext} className="slider-next">
-//         <i className="fa-solid fa-chevron-right"></i>
-//       </button>
-//       <ul className="slider__ul">
-//         {listImgs?.map((url, index) => (
-//           <li
-//             className={`slider-img-container-child ${index === indexImg && 'slider-border'
-//               }`}
-//             onClick={() => setIndexImg(index)}
-//             key={url}
-//           >
-//             <img className="slider-img-child" src={url} alt="" />
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default SliderImg;
-
 import React, { useState } from 'react';
 import './styles/sliderImg.css';
 
@@ -91,23 +28,23 @@ const SliderImg = ({ listImgs }) => {
 
   return (
     <div className="slider">
-      <button onClick={handleBack} className="slider-back">
-        <i className="fa-solid fa-chevron-left"></i>
+      <button onClick={handleBack} className="slider__back">
+        <i className="fa-solid fa-chevron-left "></i>
       </button>
       <div style={styleContainer} className="slider-container">
         {listImgs?.map((url) => (
-          <div className="slider-img-container" key={url.url}>
-            <img className="slider-img" src={url.url} alt="product-img" />
+          <div className="slider__img-container" key={url.url}>
+            <img className="slider__img" src={url.url} alt="product-img" />
           </div>
         ))}
       </div>
-      <button onClick={handleNext} className="slider-next">
-        <i className="fa-solid fa-chevron-right"></i>
+      <button onClick={handleNext} className="slider__next">
+        <i className="fa-solid fa-chevron-right "></i>
       </button>
       <ul className="slider__ul">
         {listImgs?.map((url, index) => (
           <li
-            className={`slider-img-container-child ${index === indexImg && 'slider-border'
+            className={`slider-img-container-child ${index === indexImg && 'slider__border'
               }`}
             onClick={() => setIndexImg(index)}
             key={url.id}
